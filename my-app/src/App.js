8109876b8e343b7127944e4766535bf167b82dca
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-03-04 15:44:13 +0000

    Add stateful component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import FunctionalGreeting from './components/FunctionalGreeting';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+import StatefulGreeting from './components/StatefulGreeting';
 
 function App() {
   return (
     <div className="App">
-      <FunctionalGreeting />
-      <FunctionalGreetingWithProps greeting="Nice to meet you!" name="Mike" age="32" />
+      {/* <FunctionalGreeting />
+      <FunctionalGreetingWithProps greeting="Nice to meet you!" name="Mike" age="32" /> */}
+      < StatefulGreeting greeting="I'm a stateful class component" name="Kelly" />
     </div>
   );
 }
