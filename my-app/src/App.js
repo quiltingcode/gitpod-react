bbab68f387969b2e9744bcabe5e846a558ca9f81
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-03-06 09:26:20 +0000

    Add callback function

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import FunctionalGreeting from './components/FunctionalGreeting';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
 import StatefulGreeting from './components/StatefulGreeting';
+import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
 
 function App() {
   return (
     <div className="App">
       {/* <FunctionalGreeting />
       <FunctionalGreetingWithProps greeting="Nice to meet you!" name="Mike" age="32" /> */}
-      < StatefulGreeting greeting="I'm a stateful class component" name="Kelly" />
+      {/* < StatefulGreeting greeting="I'm a stateful class component" name="Kelly" /> */}
+      < StatefulGreetingWithCallback greeting="I'm a stateful class component" name="Kelly" />
     </div>
   );
 }
