bff5ed679195d0602eb575eff9d7d7e4552c3cb1
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-03-06 13:56:00 +0000

    Create events functional and class components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,6 +5,8 @@ import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProp
 import StatefulGreeting from './components/StatefulGreeting';
 import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
 import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
+import EventsFunctional from './components/EventsFunctional';
+import EventsClass from './components/EventsClass';
 
 function App() {
   return (
@@ -13,7 +15,9 @@ function App() {
       <FunctionalGreetingWithProps greeting="Nice to meet you!" name="Mike" age="32" /> */}
       {/* < StatefulGreeting greeting="I'm a stateful class component" name="Kelly" /> */}
       {/* < StatefulGreetingWithCallback greeting="I'm a stateful class component" name="Kelly" /> */}
-      < StatefulGreetingWithPrevState greeting="I'm a stateful class component" name="Kelly" />
+      {/* < StatefulGreetingWithPrevState greeting="I'm a stateful class component" name="Kelly" /> */}
+      <EventsFunctional />
+      <EventsClass />
     </div>
   );
 }
