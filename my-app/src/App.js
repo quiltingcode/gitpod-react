3fe576dbb0a6becf686b4043879a521289a3817f
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-03-06 10:59:57 +0000

    Add toggle function

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,6 +4,7 @@ import FunctionalGreeting from './components/FunctionalGreeting';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
 import StatefulGreeting from './components/StatefulGreeting';
 import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
+import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
       {/* <FunctionalGreeting />
       <FunctionalGreetingWithProps greeting="Nice to meet you!" name="Mike" age="32" /> */}
       {/* < StatefulGreeting greeting="I'm a stateful class component" name="Kelly" /> */}
-      < StatefulGreetingWithCallback greeting="I'm a stateful class component" name="Kelly" />
+      {/* < StatefulGreetingWithCallback greeting="I'm a stateful class component" name="Kelly" /> */}
+      < StatefulGreetingWithPrevState greeting="I'm a stateful class component" name="Kelly" />
     </div>
   );
 }
