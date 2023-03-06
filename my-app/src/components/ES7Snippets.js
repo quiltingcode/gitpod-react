d57f9602ff37a7f9039c17a42e6eb3d9107951d1
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-03-06 13:44:39 +0000

    Add es7 snippets

---
+++ my-app/src/components/ES7Snippets.js
@@ -0,0 +1,58 @@
+// Type rfce and then tab for a functional
+// component
+
+// import React from 'react'
+
+// function ES7Snippets() {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+// Type rafce and then tab - same as above but 
+// with the component as an arrow function
+
+// import React from 'react'
+
+// const ES7Snippets = () => {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets 
+
+// Type rce to create and export a class component
+
+// import React, { Component } from 'react'
+
+// export class ES7Snippets extends Component {
+//   render() {
+//     return (
+//       <div>ES7Snippets</div>
+//     )
+//   }
+// }
+
+// export default ES7Snippets
+
+// Type rconst to create a constructor
+
+// constructor(props) {
+//   super(props)
+
+//   this.state = {
+//      first
+//   }
+// }
+
+// Type clg to log something to the console
+
+// console.log(first)
+
+// Type clo to log an object along with a name
+// as a String
+
+// console.log('first', first)
