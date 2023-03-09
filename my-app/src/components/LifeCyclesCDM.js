89d98cabc9375976862f1487184438fb001838a7
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-03-09 16:52:56 +0000

    Add timeout and component did mount methods

--- my-app/src/components/LifeCyclesCDM.js
+++ my-app/src/components/LifeCyclesCDM.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react'
 
 export class LifeCyclesCDM extends Component {
+
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         data: 'loading'
+      }
+    }
+
+    getData() {
+        setTimeout(() => {
+            this.setState({
+                data: 'loaded'
+            })
+        }, 3000)
+    }
+
+    componentDidMount() {
+        this.getData()
+    }
+
   render() {
     return (
-      <div>LifeCycles</div>
+      <div>
+        <h1>{this.state.data}</h1>
+      </div>
     )
   }
 }
