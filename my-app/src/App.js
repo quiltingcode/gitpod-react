ba0dc733527fc8ead6a7c9386a83ae8fdb8a5255
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-03-10 15:55:05 +0000

    Add search filter component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -18,6 +18,8 @@ import LifeCyclesCDU from './components/LifeCyclesCDU';
 import LifeCyclesCWU from './components/LifeCyclesCWU';
 import ControlledForm from './components/ControlledForm';
 import UncontrolledForm from './components/UncontrolledForm';
+import SearchBar from './components/SearchBar';
+
 
 function App() {
   return (
@@ -39,7 +41,8 @@ function App() {
       {/* <LifeCyclesCDU /> */}
       {/* <LifeCyclesCWU /> */}
       {/* <ControlledForm /> */}
-      <UncontrolledForm />
+      {/* <UncontrolledForm /> */}
+      <SearchBar />
     </div>
   );
 }
