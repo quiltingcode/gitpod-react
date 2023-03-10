ebeb73cebda4cc9dc10ef87cb5e1d0c5efc7d162
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-03-10 12:21:38 +0000

    Add uncontrolled form component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -17,6 +17,7 @@ import LifeCyclesCDM from './components/LifeCyclesCDM';
 import LifeCyclesCDU from './components/LifeCyclesCDU';
 import LifeCyclesCWU from './components/LifeCyclesCWU';
 import ControlledForm from './components/ControlledForm';
+import UncontrolledForm from './components/UncontrolledForm';
 
 function App() {
   return (
@@ -37,7 +38,8 @@ function App() {
       {/* <LifeCyclesCDM /> */}
       {/* <LifeCyclesCDU /> */}
       {/* <LifeCyclesCWU /> */}
-      <ControlledForm />
+      {/* <ControlledForm /> */}
+      <UncontrolledForm />
     </div>
   );
 }
