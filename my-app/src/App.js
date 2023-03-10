a846719540f0ec6021461fa90024abaeb1496270
Kelly Hutchison <kellyhutchison@hotmail.com>
2023-03-10 09:31:04 +0000

    Add componentdidupdate components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -14,6 +14,7 @@ import NestingComponents from './components/NestingComponents';
 import MethodsAsPropsParent from './components/MethodsAsPropsParent.js';
 import RenderingLists from './components/RenderingLists';
 import LifeCyclesCDM from './components/LifeCyclesCDM';
+import LifeCyclesCDU from './components/LifeCyclesCDU';
 
 function App() {
   return (
@@ -31,7 +32,8 @@ function App() {
       {/* <NestingComponents/> */}
       {/* <MethodsAsPropsParent/> */}
       {/* <RenderingLists /> */}
-      <LifeCyclesCDM />
+      {/* <LifeCyclesCDM /> */}
+      <LifeCyclesCDU />
     </div>
   );
 }
